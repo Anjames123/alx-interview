24e6868efd4767e6ee4e87f42c81c2f02ea35702
Anjames123 <ajibadejames19@gmail.com>
2023-12-05 18:45:40 +0000

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,40 +1,21 @@
 #!/usr/bin/python3
-"""
-This module contains a function to solve the lockboxes algorrithm problem
-"""
+""" Lockboxes """
+
 
 
 def canUnlockAll(boxes):
-    """
-    Determines if all the boxes can be opened
+    
+    
+    """ method that determines if all the boxes can be opened """
 
-    Args:
-        boxes (list): list of boxes
-
-    Return:
-        (bool) True if all boxes can be opened, false otherwise
-    """
-    if not boxes or len(boxes) == 1:
+    
+    if len(boxes) == 0:
+        return False
+    keys = [0]
+    for key in keys:
+        for box in boxes[key]:
+            if box not in keys and box < len(boxes):
+                keys.append(box)
+    if len(keys) == len(boxes):
         return True
-
-    current_box = 0
-    boxes_keys = {}
-    next_box = True
-
-    while next_box:
-        boxes_keys[current_box] = True
-        for key in boxes[current_box]:
-            if key not in boxes_keys.keys():
-                boxes_keys[key] = False
-
-        if len(boxes_keys) == len(boxes):
-            return True
-
-        next_box = False
-        for key, unlocked in boxes_keys.items():
-            if (not unlocked) and (key < len(boxes) and key >= 0):
-                next_box = True
-                current_box = key
-                break
-
     return False
