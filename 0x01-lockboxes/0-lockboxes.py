1aaf78e729d50b6b18cc120c320e8f5ffa46957e
Anjames123 <ajibadejames19@gmail.com>
2023-12-07 14:29:01 -0800

    tasks

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,32 +1,22 @@
 #!/usr/bin/python3
-"""
-This module contains a function to solve the lockboxes algorrithm problem
-"""
+'''
+You have n number of locked boxes in front of you. Each box is numbered
+sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
+Write a method that determines if all the boxes can be opened.
+'''
 
 
 def canUnlockAll(boxes):
-    """
-    Determines if all the boxes can be opened
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True
+    keys = boxes[0]
 
-    Args:
-        boxes (list): list of boxes
+    for i in range(n):
+        if unlocked[i]:
+            for key in boxes[i]:
+                if key < n and not unlocked[key]:
+                    unlocked[key] = True
+                    keys.extend(boxes[key])
 
-    Return:
-        (bool) True if all boxes can be opened, false otherwise
-    """
-    if not boxes or len(boxes) == 1:
-        return True
-
-    total_boxes = len(boxes)
-    visited_boxes = set()
-    stack = [0]  # Start with the first box
-
-    while stack:
-        current_box = stack.pop()
-        visited_boxes.add(current_box)
-
-        for key in boxes[current_box]:
-            if key not in visited_boxes and 0 <= key < total_boxes:
-                stack.append(key)
-
-    return len(visited_boxes) == total_boxes
+    return all(unlocked)
